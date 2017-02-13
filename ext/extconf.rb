22bb0aff637283713538a31fb99093cd3af331b3
pioz <enrico@megiston.it>
2017-02-13 16:30:39 +0100

    fix extconf for new ruby versions

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,31 +2,27 @@ ENV['RC_ARCHS'] = '' if RUBY_PLATFORM =~ /darwin/
 
 require 'mkmf'
 
-LIBDIR      = Config::CONFIG['libdir']
-INCLUDEDIR  = Config::CONFIG['includedir']
-
-HEADER_DIRS = [
-  # First search /opt/local for macports
+INCLUDE_DIRS = [
+  # Check the ruby install locations
+  RbConfig::CONFIG['includedir'],
+  # First earch /opt/local for macports
   '/opt/local/include',
   # Then search /usr/local for people that installed from source
   '/usr/local/include',
-  # Check the ruby install locations
-  INCLUDEDIR,
   # Finally fall back to /usr
   '/usr/include',
 ]
 
 LIB_DIRS = [
+  # Check the ruby install locations
+  RbConfig::CONFIG['libdir'],
   # First search /opt/local for macports
   '/opt/local/lib',
   # Then search /usr/local for people that installed from source
   '/usr/local/lib',
-  # Check the ruby install locations
-  LIBDIR,
   # Finally fall back to /usr
   '/usr/lib',
 ]
 
-dir_config('fuzzy_search', HEADER_DIRS, LIB_DIRS)
-
+#dir_config('fuzzy_search', HEADER_DIRS, LIB_DIRS)
 create_makefile('fuzzy_search/fuzzy_search')
